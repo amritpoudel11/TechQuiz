36cfb9e425507df71d1f9d6b9aa0a538a5c7de7b
Amrit Poudel <amritpoudel11@gmail.com>
2020-02-02 20:59:02 +0100

    initial layout of computer quiz

---
+++ src/components/Header.vue
@@ -0,0 +1,8 @@
+<template>
+    <div>
+        <b-nav>
+            <b-nav-item disabled>Nice Quiz App</b-nav-item>
+            <b-nav-item disabled>Counter: 2/10</b-nav-item>
+    </b-nav>
+    </div>
+</template>
---
+++ src/components/QuestionBox.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <b-jumbotron>
+           
+    <template v-slot:lead>
+        5. One question here ?
+    </template>
+
+    <hr class="my-4">
+
+    <p>
+      List of answers
+    </p>
+
+    <b-button variant="primary" href="#">Submit</b-button>
+    <b-button variant="success" href="#">Next</b-button>
+        </b-jumbotron>
+    </div>
+</template>
